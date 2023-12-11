871be3ca77de9e20b620b5d8e223c2cd4b5b96e8
Carlos Andres Ramirez <CarlosAndresRamirez@users.noreply.github.com>
2023-12-11 22:27:36 +0900

    clang crash
    clang crash

---
+++ clang-2023-12-11/segfault-stack-overflow-02.c
@@ -0,0 +1,189 @@
+vtch_tu\83
+  prA"?e[2 \>
+#define FO \>
+#define FO(void)                                                               \
+  u"vtruin(void)u" void truinvtch_tu                                           \
+  }
+  prA"\\80>
+#define \96tch_tu }
+  prA"\e[2J\>
+#define FO(void) void trui < st
+ie_d#\8Br\00\00\00\80ude$<btdho.h
+
+# 0 "n\00\00\00v|
+Z\91\97 a\Z[G");
+9r_)efi FO(voide FOvtur
+{
+  ? ? ? ?
+
+        ##########\FE########## #$##L");
+        :r_efi FO(\8Ar{, ? ? !? ? = ncltK io = ? ? vtch_tu } prA "\e[2J\>
+#define FO(void)void tr128d truin(vui <st
+                                  ie_d #\8Br\00\00\00\80ude$ < btdh\00\00h
+
+# 0 "n\00\00\00v|
+                                                         Z\91\97 a\d[L !);
+  9r_2J\>
+# 0i tr128drn; f
+      FO #i\BAA "\e[2I\)__covarian\00\80\FF\FF9'rvoide FOvtur
+  {
+    ? ? ? ?
+
+          $nvtur {
+      ? ? ? ? ? ? ? ? ? ? ?\80\FF\FF\FF vtur {
+        ? ? ? ? void trr\FF_\80lignof\FF\FFvA "\e[2I\)__covarian\00\80\FF\FF9'rvoidoid
+        {
+          ;
+          ;
+          ;
+          ;.o#999
+tdi99\80\00\00\0099999=98 a\e #includeAvoR<<\FF\80<<<<<<<<<s\AD\AD\AD\AD\AD\E8\FBsZZZZZZZin(void)
+{Z\91\97r\00\00 \00# rede <Zt|
+
+# 0 "n2|
+
+#tur
+#define FO(void)vopd0F)(void#void truin(vooo>>oooooooooooot
+z
+voif=t\00\80u\00\00\00btdhoine FO(iulude <e FO(voiDDDDDDiiiiiiiin\F8f(vS=^n\
+$\C5vq=^\80\FF
+$Ām
+extern-linux-gnu/13/../.d/../../x8_tu}
+  prA"\e[2J\>
+#define FO(void)void tr128d truin(vui <st
+ie_d#\8Br\00\00\00\80ude$<btdh\00\00h
+
+# 0 "n\00\00\00v|
+Z\91\97 a\d[L!);
+9r_2J\>
+# 0i tr128drn; f
+FO#i\BAA"\e[2I\)__covarian\00\80\FF\FF9'rvoide FOvtur
+{
+            ? ? ? ?
+
+                  $nvtur {
+              ? ? ? ? ? ? ? ? ? ? ?\80\FF\FF\FF vtur {
+                ? ? ? ? void trr\FF_\80lignof\FF\FFvA "\e[2I\)__covarian\00\80\FF\FF9'rvoidoid
+                {
+                  ;
+                  ;
+                  ;
+                  ;.o#999
+tdi99\80\00\00\0099999=98 a\e #includeAvoR<<\FF\80<<<<<<<<<s\AD\AD\AD\AD\AD\E8\FBsruin(v in(void)
+{Z\91\97r\00\00 \00# rede <Zt|
+
+# 0 "n2|
+
+#tur
+#define FO(void)vopd0F)(void#void truin(vooo>>oooooooooooot
+z
+voif=t\00\80u\00\00\00btdhoine FO(iulude <e FO(voiDDDDDDiiiiiiiin\F8f(vS=^n\
+$\C5vq=^\80\FF
+$Ām
+extern-linux-gnu/13/../../../../x86_64(void)
+{
+                      __ac_exchange(&x, &y, &z, 0);
+}
+
+// Original com      <\t
+# 0
+
+
+b/llvm-16/bin/clang" "-cc1" "-triple" "x86_64-pc-linux-gnu" "-gmit-obj" "-mrelax-all" "-disable-free" "-clear-ast-before-backend" "-disable-llvm-verifier" "-discard-value-names" "-main-file-name" "test-01.c" "-mrelocation-model" "pic" "-pic-,evel" "2" "-pic-is-pie" "-mframe-pointer=all" "-fmath-errno" "-ffp-contract=on" "-fno-rounding-math" "-mconstructor-aliases" "-funwind-tables=2" "-target-c\B2u" "x86-64" "-tune-cpu" "gede FOvtur
+{
+                      ? ?
+                        ?
+                        ?
+
+                        ##########\FE########## #$##L");
+                        :r_efi FO(\8Ar{
+                            , ? ? !? ? = ncltK io = ? ? vtch_tu } prA "\e[2J\>
+#define FO(void)void tr128d truin(vui <st
+                                       ie_d #\8Br\00\00\00\80ude$ < btdh\00\00h
+
+# 0 "n\00\00\00v|
+                                                              Z\91\97 a\d[L !);
+                      9r_2J\>
+# 0i tr128drn; f
+                          FO #i\BAA "\e[2I\)__covarian\00\80\FF\FF9'rvoide FOvtur
+                      {
+                        ? ? ? ?
+
+                              $nvtur {
+                          ? ? ? ? ? ? ? ? ? ? ?\80\FF\FF\FF vtur {
+                            ? ? ? ? void trr\FF_\80lignof\FF\FFvA
+                                "\e[2I\)__covarian\00\80\FF\FF9'rvoidoid
+                            {
+                              ;
+                              ;
+                              ;
+                              ;.o#999
+tdi99\80\00\00\0099999=98 a\e #includeAvoR<<\FF\80<<<<<<<<<s\AD\AD\AD\AD\AD\E8\FBsruin(v in(void)
+{Z\91\97r\00\00 \00# rede <Zt|
+
+# 0 "n2|
+
+#tur
+#define FO(void)vopd0F)(void#void truin(vooo>>oooooooooooot
+z
+voif=t\00\80u\00\00\00btdhoine FO(iulude <e FO(voiDDDDDDiiiiiiiin\F8f(vS=^n\
+$\C5vq=^\80\FF
+$Ām
+extern-linux-gnu/13/../../../../x86_64(void)
+{
+                                  __ac_exchange(&x, &y, &z, 0);
+}
+
+// Original com      <\t?
+void trr\FF_\80lignof\FF\FFvA"\e[2I\)__covarian\00\80\FF\FF9'rvoidoid
+{
+                                  ;
+                                  ;
+                                  ;
+                                  ;.o#999
+tdi99\80\00\00\0099999=98 a\e #includeAvoR<<\FF\80<<<<<<<<<s\AD\AD\AD\AD\AD\E8\FBsruin(v in(void)
+{Z\91\97r\00\00 \00# rede <Zt|
+
+# 0 "n2|
+
+#tur
+#define FO(void)vopd0F)(void#void truin(vooo>>oooooooooooot
+z
+voif=t\00\80u\00\00\00btdhoine FO(iulude <e FO(voiDDDDDDiiiiiiiin\F8f(vS=^n\
+$\C5vq=^\80\FF
+$Ām
+extern-linux-gnu/13/../../../../x86_64(void)
+{
+                                      __ac_exchange(&x, &y, &z, 0);
+}
+
+// Original com      <\t
+# 0
+
+
+b/llvm-16/bin/clang" "-cc1" "-triple" "x86_64-pc-linux-gnu" "-gmit-obj" "-mrelax-all" "-disable-free" "-clear-ast-before-backend" "-disable-llvm-verifier" "-discard-value-names" "-main-file-name" "test-01.c" "-mrelocation-model" "pic" "-pic-,evel" "2" "-pic-is-pie" "-mframe-pointer=all
+# 0
+
+
+b/llvm-16/bin/clang" "-cc1" "-triple" "x86_64-pc-linux-gnu" "-gmit-obj" "-mrelax-all" "-disable-free" "-clear-ast-before-backend" "-disable-llvm-verifier" "-discard-value-names" "-main-file-NIL");
+9@rvoooooUoo[L");
+9 rv\FFooooio.h?
+Znt a\FF\80\80L"@oo++++++_ExtInt_tu}
+ t
+ie_ma
+
+ss r[ooooosssssss++++++++#incl\00\00e <s prA"\e\00\00\00\80>
+#define FO(void)void t(vo <st(void)voidine FO(void)void truin(vo <st
+dude$<>
+\00#defi
+
+# 0 "n\00\00\00v|
+Z\91\97 FO(vo a\Z[Lvd)void t(t
+i
+fine FO()voi)void tname" "test-01.c" "-mrelocation-model" "pic" "-pic-,evel" "2" "-pic-is-pie" "-mframe-pointer=all" "-fmath-errno" "-ffp-contract=on" "-fno-rounding-math" "-mconstructor-aliases" "-funwind-tables=2" "-target-c\B2u" "x86-64" "-tune-cpu" "generic" "-mllvm"\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4ble-fixed-error-as-warning" "ar-ast--debugger-tuning=gdb" "-fcoverage-\00\00\00ilation-dir=/home/andres/fuzz/interesing_inpunteresing_inputs/c_src" "-resource-dir" "/usr/lib/llvm-16/lib/clange{
+ /16" "-internal-isystem" "/usr/lib/lneric" "-mllvm"\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4\B4ble-fixed-error-as-warning" "ar-ast--debugger-tuning=gdb" "-fcoverage-\00\00\00ilation-dir=/home/andres/fuzz/interesing_inpunteresing_inputs/c_src" "-resource-dir" "/usr/lib/llvm-16/lib/clange{
+ /16" "-internal-isystem" "/usr/lib/llvm-16/lib/clang/16/include" "-internal-isystem" "/usr/local/include" "-interna\93-isystem" "/usr/bin/../lib/gCc/x86_64 char x[], y[], z[];
+
+void
+func -linux-gnu/include" "-i\FF 
+  893@458943type))))))))))))))))))))x86_64-linux-gnu" "-internal-externc-isystem" "/include" "-internal-externc-
