6534c261407854501c2eaaa6539d2e951b94a7fc
Carlos Andres Ramirez <CarlosAndresRamirez@users.noreply.github.com>
2025-10-27 11:35:51 +0900

    Code handling issues in NASA's trick
    Code handling issues in NASA's trick

---
+++ NASA-trick-2025-10/crash-1-2.c
@@ -0,0 +1,13 @@
+
+#define MAX 0
+int x = 1;
+#ifndef MAS_ROVER_MODEL_H
+namespace MarsSim {
+struct rStase {
+double pion[3];
+double v;locity; double ing; vstruct Roverne MAolatile int sye;
+    double cpmp;
+list;
+};
+}
+#endif
---
+++ NASA-trick-2025-10/issue-1-3.c
@@ -0,0 +1,12 @@
+
+#define MAX 0
+int x = 1;
+#ifndef MAS_ROVER_MODEL_H
+namespace MarsSim {
+struct RoversSim {
+struct RoverStat RoverState {
+doublble ing; volatile int sye;
+    double cpmp;
+list;
+};
+}û#endif
